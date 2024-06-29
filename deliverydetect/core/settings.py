2c137db2074585c23beccab4d78a9bef6f62f2cb
Ilya Kuznetsov <ilkztsff@gmail.com>
2024-06-29 17:19:00 +0300

    Add settings

---
+++ deliverydetect/core/settings.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+from os import environ
+
+
+load_dotenv()
+
+BOT_TOKEN = environ['BOT_TOKEN']
+HOST = environ['HOST']
+WEBHOOK_PATH: str = f'bot/{BOT_TOKEN}'
+WEB_SERVER_URL: str = environ['WEB_SERVER_URL']
+WEBHOOK_URL: str = f'{WEB_SERVER_URL}{"/" if not WEB_SERVER_URL.endswith("/") else ""}{WEBHOOK_PATH}'
+
+REDIS_HOST: str = environ.get('REDIS_HOST', 'localhost')
+REDIS_PORT: str = environ['REDIS_PORT']
+REDIS_PASSWORD: str = environ['REDIS_PASSWORD']
+
+POSTGRES_PORT: str = environ['POSTGRES_PORT']
+POSTGRES_USER: str = environ['POSTGRES_USER']
+POSTGRES_PASSWORD: str = environ['POSTGRES_PASSWORD']
+POSTGRES_DB: str = environ['POSTGRES_DB']
