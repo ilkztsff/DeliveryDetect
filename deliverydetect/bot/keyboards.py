6dba8a8d1387ce5e52b5eecc8f737ef413ac7c96
Ilya Kuznetsov <ilkztsff@gmail.com>
2024-07-03 17:05:16 +0300

    Fix keyboard

--- deliverydetect/bot/keyboards.py
+++ deliverydetect/bot/keyboards.py
@@ -1,7 +1,9 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from deliverydetect.models.callback import RoleCallback
+from deliverydetect.core.enums import Role
 
 
 choose_role_kb = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text='Я заказчик', callback_data='courier')],
-    [InlineKeyboardButton(text='Я курьер', callback_data='customer')]
+    [InlineKeyboardButton(text='Я заказчик', callback_data=RoleCallback(role=Role.CUSTOMER).pack())],
+    [InlineKeyboardButton(text='Я курьер', callback_data=RoleCallback(role=Role.COURIER).pack())]
 ])
