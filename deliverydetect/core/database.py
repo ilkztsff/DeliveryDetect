643e09f8c6d78a1f6caa03f3155e65f90e1a2969
Ilya Kuznetsov <ilkztsff@gmail.com>
2024-07-03 14:37:31 +0300

    Fix redis client

--- deliverydetect/core/database.py
+++ deliverydetect/core/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.asyncio import create_async_engine
-from redis import Redis
+from redis.asyncio import Redis
 
 from deliverydetect.core.settings import DATABASE_URL, REDIS_HOST, REDIS_PASSWORD, REDIS_PORT
 
