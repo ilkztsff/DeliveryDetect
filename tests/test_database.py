fa2de87e31bbd121f04449c5071ce0f64d7ae050
Ilya Kuznetsov <ilkztsff@gmail.com>
2024-07-03 14:37:34 +0300

    Fix tests

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,9 +1,11 @@
 from redis import Redis
+import pytest
 
 
-def test_redis_connection(redis_client: Redis) -> None:
-    assert redis_client.ping()
-    assert redis_client.set('foo', 'bar')
-    assert redis_client.get('foo') == 'bar'
-    assert redis_client.delete('foo')
-    redis_client.close()
+@pytest.mark.asyncio
+async def test_redis_connection(redis_client: Redis) -> None:
+    assert await redis_client.ping()
+    assert await redis_client.set('foo', 'bar')
+    assert await redis_client.get('foo') == 'bar'
+    assert await redis_client.delete('foo')
+    await redis_client.aclose()
