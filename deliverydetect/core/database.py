91ceb971aa344891bed948ac83abac4adf7121f7
Ilya Kuznetsov <ilkztsff@gmail.com>
2024-07-04 16:02:08 +0300

    Add sessionmaker

--- deliverydetect/core/database.py
+++ deliverydetect/core/database.py
@@ -1,4 +1,5 @@
 from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker
 from redis.asyncio import Redis
 
 from deliverydetect.core.settings import DATABASE_URL, REDIS_HOST, REDIS_PASSWORD, REDIS_PORT
@@ -6,3 +7,4 @@ from deliverydetect.core.settings import DATABASE_URL, REDIS_HOST, REDIS_PASSWOR
 
 redis = Redis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD, decode_responses=True)
 engine = create_async_engine(DATABASE_URL)
+sessionmaker = async_sessionmaker(engine)
