4f9e274ef20dbb803d6e13e2609fb00029852caf
Ilya Kuznetsov <ilkztsff@gmail.com>
2024-07-04 11:44:05 +0300

    Update migrations

--- migrations/env.py
+++ migrations/env.py
@@ -1,29 +1,21 @@
-from logging.config import fileConfig
-
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
 from alembic import context
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
+from logging.config import fileConfig
+
+from deliverydetect.models.database import Base
+from deliverydetect.models.database import *  # noqa
+from deliverydetect.core.settings import DATABASE_URL
+
+
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+config.set_main_option('sqlalchemy.url', f'{DATABASE_URL}?async_fallback=True')
+target_metadata = Base.metadata
 
 
 def run_migrations_offline() -> None:
