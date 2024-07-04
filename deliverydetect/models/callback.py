5c5bbf99cc2f0d394ad4088579471601c1906561
Ilya Kuznetsov <ilkztsff@gmail.com>
2024-07-04 11:44:35 +0300

    Update models

--- deliverydetect/models/callback.py
+++ deliverydetect/models/callback.py
@@ -1,7 +1,10 @@
 from aiogram.filters.callback_data import CallbackData
-
-from deliverydetect.core.enums import Role
+from deliverydetect.core.enums import Role, CourierMenuState
 
 
 class RoleCallback(CallbackData, prefix='role'):
     role: Role
+
+
+class EditCourierInfoMenuCallback(CallbackData, prefix='courier'):
+    state: CourierMenuState
