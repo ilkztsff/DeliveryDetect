d5a4144943bc7d793e4d189c1d3d99a323f751f1
Ilya Kuznetsov <ilkztsff@gmail.com>
2024-07-09 17:17:01 +0300

    Add flood middleware

--- deliverydetect/bot/middlewares.py
+++ deliverydetect/bot/middlewares.py
@@ -1,6 +1,9 @@
+from aiogram import Bot
 from aiogram.enums import ChatType
 from aiogram.dispatcher.middlewares.base import BaseMiddleware
 from aiogram.types import TelegramObject
+
+from redis.asyncio import Redis
 
 from typing import Callable, Awaitable, Union, Dict, Any
 
@@ -17,3 +20,34 @@ class PrivateChatsMiddleware(BaseMiddleware):
             return None
 
         return await handler(event, data)
+
+
+class FloodMiddleware(BaseMiddleware):
+    def __init__(self, redis: Redis, bot: Bot) -> None:
+        self.redis = redis
+        self.bot = bot
+
+    async def __call__(
+            self, 
+            handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+            event: TelegramObject,
+            data: Dict[str, Any]
+        ) -> Any:
+
+        event_model = event.model_dump()
+
+        try:
+            user = event_model['from_user']['id']
+        except KeyError:
+            return None
+
+        check = await self.redis.get('user')
+
+        if check is None:
+            await self.redis.set(user, 1, 5)
+            return await handler(event, data)
+        elif int(check) == 1:
+            await self.redis.set(user, 0, 5)
+            return await self.bot.send_message('Не спамить!! Вы заблокированы на 5 секунд', chat_id=event_model['chat']['id'])
+        else:
+            return None
