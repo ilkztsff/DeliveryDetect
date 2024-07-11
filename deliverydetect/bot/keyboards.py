666ac7a33bf3ae49f07b791249fdba8e7c7e4a65
Ilya Kuznetsov <ilkztsff@gmail.com>
2024-07-11 15:35:37 +0300

    Update keyboards

--- deliverydetect/bot/keyboards.py
+++ deliverydetect/bot/keyboards.py
@@ -1,6 +1,8 @@
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton
-from deliverydetect.models.callback import RoleCallback, EditCourierInfoMenuCallback
-from deliverydetect.core.enums import Role, CourierMenuState
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+from deliverydetect.models.callback import RoleCallback, EditCourierInfoMenuCallback, TransportCallback, ConfirmationCallback
+from deliverydetect.core.enums import Role, CourierMenuState, Transport
+from deliverydetect.core.settings import _
 
 
 choose_role_ikb = InlineKeyboardMarkup(inline_keyboard=[
@@ -9,29 +11,25 @@ choose_role_ikb = InlineKeyboardMarkup(inline_keyboard=[
 ])
 
 
-yes_no_kb = ReplyKeyboardMarkup(
-    keyboard=[
-        [KeyboardButton(text='✅ Да')],
-        [KeyboardButton(text='❌ Нет')]
-    ],
-    one_time_keyboard=True,
-    resize_keyboard=True
+yes_no_ikb = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='✅ Yes', callback_data=ConfirmationCallback(confirm=True).pack())],
+        [InlineKeyboardButton(text='❌ No', callback_data=ConfirmationCallback(confirm=False).pack())]
+    ]
 )
 
-
 edit_courier_info_ikb = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text='Имя', callback_data=EditCourierInfoMenuCallback(state=CourierMenuState.NAME).pack())],
-    [InlineKeyboardButton(text='Телефон', callback_data=EditCourierInfoMenuCallback(state=CourierMenuState.CONTACT).pack())],
-    [InlineKeyboardButton(text='Наличие термосумки', callback_data=EditCourierInfoMenuCallback(state=CourierMenuState.HAS_THERMAL_BAG).pack())],
-    [InlineKeyboardButton(text='Транспорт', callback_data=EditCourierInfoMenuCallback(state=CourierMenuState.TRANSPORT).pack())]
+    [InlineKeyboardButton(text=_('Имя'), callback_data=EditCourierInfoMenuCallback(state=CourierMenuState.NAME).pack())],
+    [InlineKeyboardButton(text=_('Телефон'), callback_data=EditCourierInfoMenuCallback(state=CourierMenuState.CONTACT).pack())],
+    [InlineKeyboardButton(text=_('Наличие термосумки'), callback_data=EditCourierInfoMenuCallback(state=CourierMenuState.HAS_THERMAL_BAG).pack())],
+    [InlineKeyboardButton(text=_('Транспорт'), callback_data=EditCourierInfoMenuCallback(state=CourierMenuState.TRANSPORT).pack())]
 ])
 
-choose_transport_kb = ReplyKeyboardMarkup(
-    one_time_keyboard=True,
-    keyboard=[
-        [KeyboardButton(text='🚴 Велосипед')],
-        [KeyboardButton(text='🚘 Автомобиль')],
-        [KeyboardButton(text='🏍 Мотоцикл')],
-        [KeyboardButton(text='🧍 Я хожу пешком')]
+choose_transport_ikb = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text=_('🚴 Bike'), callback_data=TransportCallback(transport=Transport.BIKE).pack())],
+        [InlineKeyboardButton(text=_('🚘 Car'), callback_data=TransportCallback(transport=Transport.CAR).pack())],
+        [InlineKeyboardButton(text=_('🏍 Motorbike'), callback_data=TransportCallback(transport=Transport.MOTORBIKE).pack())],
+        [InlineKeyboardButton(text=_('🧍 I\'m going on foot'), callback_data=TransportCallback(transport=Transport.FEET).pack())]
     ]
 )
